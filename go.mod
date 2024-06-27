17926d6db57dd123fca67770d5ca413fadafd2c6
dxj <1718874198@qq.com>
2024-06-27 11:40:27 +0800

    [update]Demo test

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
-module "script-go"
+module script-go
 
 go 1.18
+
+require github.com/daku10/go-lz-string v0.0.6 // indirect
--- src/application/applet/Demo/Demo.go
+++ src/application/applet/Demo/Demo.go
@@ -1,6 +1,8 @@
 package Demo
 
 import (
+	"encoding/base64"
+	lzstring "github.com/daku10/go-lz-string"
 	"script-go/src/application/pojo/dto/Log"
 	"script-go/src/application/util/LogUtil"
 )
@@ -16,7 +18,39 @@ func (d *Demo) test() {
 	LogUtil.LoggerLine(Log.Of("Demo", "test", "msg", "Hello world!"))
 }
 
+func (d *Demo) test1() {
+	content := "Hello world"
+	contentBase64 := base64.StdEncoding.EncodeToString([]byte(content))
+	LogUtil.LoggerLine(Log.Of("ApplicationTest", "Test1", "contentBase64", contentBase64))
+	contentBase64 = base64.StdEncoding.EncodeToString([]byte(contentBase64))
+	LogUtil.LoggerLine(Log.Of("ApplicationTest", "Test1", "contentBase64", contentBase64))
+
+	compressedStr, err := lzstring.CompressToEncodedURIComponent(contentBase64)
+	if err != nil {
+		LogUtil.LoggerLine(Log.Of("ApplicationTest", "Test1", "err", err))
+	}
+	LogUtil.LoggerLine(Log.Of("ApplicationTest", "Test1", "compressedStr", compressedStr))
+
+	decompressedStr, err := lzstring.DecompressFromEncodedURIComponent(compressedStr)
+	if err != nil {
+		LogUtil.LoggerLine(Log.Of("ApplicationTest", "Test1", "err", err))
+	}
+	LogUtil.LoggerLine(Log.Of("ApplicationTest", "Test1", "decompressedStr", decompressedStr))
+
+	message, err := base64.StdEncoding.DecodeString(decompressedStr)
+	if err != nil {
+		LogUtil.LoggerLine(Log.Of("ApplicationTest", "Test1", "err", err))
+	}
+	LogUtil.LoggerLine(Log.Of("ApplicationTest", "Test1", "string(message)", string(message)))
+	message, err = base64.StdEncoding.DecodeString(string(message))
+	if err != nil {
+		LogUtil.LoggerLine(Log.Of("ApplicationTest", "Test1", "err", err))
+	}
+	LogUtil.LoggerLine(Log.Of("ApplicationTest", "Test1", "string(message)", string(message)))
+}
+
 func Run() {
 	demo := newDemo()
-	demo.test()
+	demo.test1()
+	//demo.test()
 }
