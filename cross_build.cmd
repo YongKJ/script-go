52317b0e8a553a128c6bd8abf296db1d50347abe
dxj <dxj1718874198@gmail.com>
2024-07-04 22:20:55 +0800

    [update]cross build

--- cross_build.cmd
+++ cross_build.cmd
@@ -1,30 +1,28 @@
 :: windows build
 
-:: SET GOARCH=amd64
-:: SET GOOS=windows
-:: SET CGO_ENABLED=1
+SET GOARCH=amd64
+SET GOOS=windows
+SET CGO_ENABLED=1
 
-:: rm -rf D:/Document/MyCodes/Worker/Go/script-go/dist
-:: cd D:/Document/MyCodes/Worker/Go/script-go
+rm -rf ./dist
 
 :: SET CC=D:\Software\scoop\apps\gcc\current\bin\gcc.exe
 :: SET CXX=D:\Software\scoop\apps\gcc\current\bin\g++.exe
 
-:: go build -o ./dist/script-go.exe
+go build -o ./dist/script-go.exe
 
 :: linux build
 
-SET GOARCH=amd64
-SET GOOS=linux
-SET CGO_ENABLED=1
+:: SET GOARCH=amd64
+:: SET GOOS=windows
+:: SET CGO_ENABLED=1
 
-rm -rf D:/Document/MyCodes/Worker/Go/script-go/dist
-cd D:/Document/MyCodes/Worker/Go/script-go
+:: rm -rf ./dist
 
 :: SET CC=D:\Software\scoop\apps\mingw\current\bin\x86_64-w64-mingw32-gcc.exe
 :: SET CXX=D:\Software\scoop\apps\mingw\current\bin\x86_64-w64-mingw32-g++.exe
 
-go build -o ./dist/script-go
+:: go build -o ./dist/script-go
 
 :: android 9 build
 
@@ -32,8 +30,7 @@ go build -o ./dist/script-go
 :: SET GOOS=android
 :: SET CGO_ENABLED=1
 
-:: rm -rf D:/Document/MyCodes/Worker/Go/script-go/dist
-:: cd D:/Document/MyCodes/Worker/Go/script-go
+:: rm -rf ./dist
 
 :: SET ANDROID_API=android28
 :: SET CC=D:\Software\Android\Sdk\ndk\27.0.11902837\toolchains\llvm\prebuilt\windows-x86_64\bin\aarch64-linux-%ANDROID_API%-clang.cmd
