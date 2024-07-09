42bb357d239ca7f84be9685172fb23e8ea27c36f
dxj <1718874198@qq.com>
2024-07-09 19:30:50 +0800

    [update]C Hello

--- src/application/applet/CHello/CHello.go
+++ src/application/applet/CHello/CHello.go
@@ -8,16 +8,25 @@ void test() {
 }
 */
 import "C"
+import (
+	"script-go/src/application/pojo/dto/Log"
+	"script-go/src/application/util/GenUtil"
+	"script-go/src/application/util/LogUtil"
+)
 
 type CHello struct {
+	msg string
 }
 
 func newCHello() *CHello {
-	return &CHello{}
+	return &CHello{
+		msg: GenUtil.GetValue("msg").(string),
+	}
 }
 
 func (c *CHello) test() {
 	C.test()
+	LogUtil.LoggerLine(Log.Of("CHello", "test", "c.msg", c.msg))
 }
 
 func Run() {
