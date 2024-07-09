1c6014e1095f05a4d01917be4067bb4bc0fe5979
dxj <1718874198@qq.com>
2024-07-09 20:26:56 +0800

    [update]Script build

--- src/application/deploy/pojo/dto/BuildConfig/BuildConfig.go
+++ src/application/deploy/pojo/dto/BuildConfig/BuildConfig.go
@@ -2,10 +2,12 @@ package BuildConfig
 
 import (
 	"script-go/src/application/util/DataUtil"
+	"script-go/src/application/util/FileUtil"
 )
 
 type BuildConfig struct {
 	appPath               string
+	appTestPath           string
 	lstGoOS               []string
 	lstGoArch             []string
 	scriptRunPattern      string
@@ -14,12 +16,24 @@ type BuildConfig struct {
 	packageImportOriginal string
 }
 
-func newBuildConfig(appPath string, lstGoOS []string, lstGoArch []string, scriptRunPattern string, scriptRunOriginal string, packageImportPattern string, packageImportOriginal string) *BuildConfig {
-	return &BuildConfig{appPath: appPath, lstGoOS: lstGoOS, lstGoArch: lstGoArch, scriptRunPattern: scriptRunPattern, scriptRunOriginal: scriptRunOriginal, packageImportPattern: packageImportPattern, packageImportOriginal: packageImportOriginal}
+func newBuildConfig(appPath string, appTestPath string, lstGoOS []string, lstGoArch []string, scriptRunPattern string, scriptRunOriginal string, packageImportPattern string, packageImportOriginal string) *BuildConfig {
+	return &BuildConfig{appPath: appPath, appTestPath: appTestPath, lstGoOS: lstGoOS, lstGoArch: lstGoArch, scriptRunPattern: scriptRunPattern, scriptRunOriginal: scriptRunOriginal, packageImportPattern: packageImportPattern, packageImportOriginal: packageImportOriginal}
 }
 
-func Of(appPath string, lstGoOS []string, lstGoArch []string, scriptRunPattern string, scriptRunOriginal string, packageImportPattern string, packageImportOriginal string) *BuildConfig {
-	return newBuildConfig(appPath, lstGoOS, lstGoArch, scriptRunPattern, scriptRunOriginal, packageImportPattern, packageImportOriginal)
+func Of(appPath string, appTestPath string, lstGoOS []string, lstGoArch []string, scriptRunPattern string, scriptRunOriginal string, packageImportPattern string, packageImportOriginal string) *BuildConfig {
+	return newBuildConfig(appPath, appTestPath, lstGoOS, lstGoArch, scriptRunPattern, scriptRunOriginal, packageImportPattern, packageImportOriginal)
+}
+
+func Get() *BuildConfig {
+	lstGoArch := []string{"amd64", "arm64", "arm", "386"}
+	lstGoOS := []string{"windows", "linux", "android", "darwin"}
+	appPath := FileUtil.GetAbsPath("src", "application", "Application.go")
+	appTestPath := FileUtil.GetAbsPath("src", "application", "ApplicationTest.go")
+	return Of(
+		appPath, appTestPath, lstGoOS, lstGoArch,
+		"", "",
+		"", "",
+	)
 }
 
 func JsonArrayToObjects(jsonArrayStr string) []*BuildConfig {
@@ -52,6 +66,14 @@ func (b *BuildConfig) AppPath() string {
 
 func (b *BuildConfig) SetAppPath(appPath string) {
 	b.appPath = appPath
+}
+
+func (b *BuildConfig) AppTestPath() string {
+	return b.appTestPath
+}
+
+func (b *BuildConfig) SetAppTestPath(appTestPath string) {
+	b.appTestPath = appTestPath
 }
 
 func (b *BuildConfig) LstGoOS() []string {
